3825590974dcf64681f36259b1cc0cd9be3bc534
zhailin <schumilin@163.com>
2014-11-05 13:43:43 +0800

    左侧边栏版本

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -1,1 +1,49 @@
-console.log('Hello World! ');
+angular.module('homeApp', ['ngMaterial'])
+    .controller('AppCtrl', function ($scope, $mdSidenav) {
+        $scope.toggleLeft = function () {
+            $mdSidenav('left').toggle();
+        };
+        $scope.todos = [{
+            face: 'images/schumi.jpg',
+            what: '缴纳罚款 100 大洋',
+            who: 'Yaochun Zhang',
+            notes: '2014-10-23 Happy Hour 迟到'
+        }, {
+            face: 'images/schumi.jpg',
+            what: '缴纳罚款 100 大洋',
+            who: 'Jiyun Han',
+            notes: '2014-10-30 Happy Hour 迟到'
+        }, {
+            face: 'images/schumi.jpg',
+            what: '缴纳罚款 100 大洋',
+            who: 'Yaochun Zhang',
+            notes: '2014-10-23 Happy Hour 迟到'
+        }, {
+            face: 'images/schumi.jpg',
+            what: '缴纳罚款 100 大洋',
+            who: 'Jiyun Han',
+            notes: '2014-10-30 Happy Hour 迟到'
+        }, {
+            face: 'images/schumi.jpg',
+            what: '缴纳罚款 100 大洋',
+            who: 'Yaochun Zhang',
+            notes: '2014-10-23 Happy Hour 迟到'
+        }, {
+            face: 'images/schumi.jpg',
+            what: '缴纳罚款 100 大洋',
+            who: 'Jiyun Han',
+            notes: '2014-10-30 Happy Hour 迟到'
+        }, {
+            face: 'images/schumi.jpg',
+            what: '缴纳罚款 100 大洋',
+            who: 'Yaochun Zhang',
+            notes: '2014-10-23 Happy Hour 迟到'
+        }, {
+            face: 'images/schumi.jpg',
+            what: '缴纳罚款 100 大洋',
+            who: 'Jiyun Han',
+            notes: '2014-10-30 Happy Hour 迟到'
+        }];
+    });
+
+angular.bootstrap(document, ['homeApp']);
